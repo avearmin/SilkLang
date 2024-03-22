417d712c57f1e2be8d4a08c984e2be80194cedf0
avearmin <arminradoncic1@gmail.com>
2024-03-22 07:34:45 -0400

    Add Lexer struct and New()
    The input and char fields should be self-explanatory. The pos and readPos represent a standard two-pointer for examining the input which ensures the lexer is able to peek ahead into the input. This is useful to determine if the lexer is currently reading a keyword or identifier, which will span multiple characters, or a single character (which is marked by readPos pointing to a whitespace/newline).
    
    The New() function creates the lexer, and is currently bare-bones. This will be expanded upon in the future.

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -1,1 +1,13 @@
 package lexer
+
+type Lexer struct {
+	input   string
+	pos     int  // current pos in input (current char)
+	readPos int  // current reading pos in input (after current char)
+	char    byte // current char being looked at
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	return l
+}
